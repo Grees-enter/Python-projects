b5b275d31dd569b2c5dcabb0cf858f9edbdec9ea
Grees-enter <92280777+Grees-enter@users.noreply.github.com>
2021-12-03 21:14:24 +0530

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -1,6 +1,6 @@
 import pyautogui, time 
 time.sleep(5)
-file = open('beescrip.txt', 'r')
+file = open('beescript.txt', 'r')
 for word in file:
   pyautogui.typewrite(word)
   pyautogui.press('enter')
